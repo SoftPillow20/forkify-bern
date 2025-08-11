1dab79dffb54e2a25c298888fa9c48590a36707e
SoftPillow20 <Nobila.Bernie19@gmail.com>
2025-08-11 21:44:01 +0800

    Cleaned config

---
+++ src/js/KEY.js
@@ -0,0 +1,1 @@
+export const KEY = '3ac17c9a-c6ae-4e5d-ad79-885b1b445728';
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,6 @@
 import { async } from 'regenerator-runtime';
-import { API_URL, RES_PER_PAGE, KEY } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
+import { KEY } from './KEY.js';
 // import { getJSON, sendJSON } from './helpers.js';
 import { AJAX } from './helpers.js';
 
