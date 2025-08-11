d7fd522d0642857ff52a6de334003591a324d394
SoftPillow20 <Nobila.Bernie19@gmail.com>
2025-08-11 21:08:08 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3ac17c9a-c6ae-4e5d-ad79-885b1b445728';
+export const MODAL_CLOSE_SEC = 2.5;
